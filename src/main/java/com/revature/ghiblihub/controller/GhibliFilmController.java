38ea9234816474b2a18697755647e939de324ae3
BrettD98 <42455018+BrettD98@users.noreply.github.com>
2021-08-26 14:09:12 -0500

    fixed tiny typo

--- src/main/java/com/revature/ghiblihub/controller/GhibliFilmController.java
+++ src/main/java/com/revature/ghiblihub/controller/GhibliFilmController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/Films")
+@RequestMapping("/films")
 public class GhibliFilmController {
 
     private final GhibliFilmService ghibliFilmService;
