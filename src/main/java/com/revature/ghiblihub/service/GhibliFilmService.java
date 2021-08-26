76e7dc359f2b1b136358bc302c0fb845560f23eb
Brett Davis <brett.davis5511@gmail.com>
2021-08-26 14:05:33 -0500

    MVC GhibliFilm Methods

--- src/main/java/com/revature/ghiblihub/service/GhibliFilmService.java
+++ src/main/java/com/revature/ghiblihub/service/GhibliFilmService.java
@@ -1,7 +1,38 @@
 package com.revature.ghiblihub.service;
 
+import com.revature.ghiblihub.models.GhibliFilm;
+import com.revature.ghiblihub.repository.GhibliFilmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GhibliFilmService {
+    private final GhibliFilmRepository ghibliFilmRepository;
+
+    @Autowired
+    public GhibliFilmService(GhibliFilmRepository ghibliFilmRepository){
+        this.ghibliFilmRepository = ghibliFilmRepository;
+    }
+
+    public GhibliFilm getFilmById(Integer id){
+        return ghibliFilmRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public GhibliFilm getFilmByTitle(String title){
+        return ghibliFilmRepository.findByTitle(title).orElseThrow(RuntimeException::new);
+    }
+
+    public List<GhibliFilm> getAllFilms(){
+        return ghibliFilmRepository.findAll();
+    }
+
+    public GhibliFilm saveFilm(GhibliFilm ghibliFilm){
+        return ghibliFilmRepository.save(ghibliFilm);
+    }
+
+    public void deleteFilm(Integer id){
+        ghibliFilmRepository.findById(id).ifPresent(ghibliFilmRepository::delete);
+    }
 }
