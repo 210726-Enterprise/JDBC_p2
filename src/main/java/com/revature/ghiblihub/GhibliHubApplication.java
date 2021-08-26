99cb792eb85674a7e0e3e22aeaf9099a78610fcc
Chris Zhang <christopher.zhang@revature.net>
2021-08-26 08:33:42 -0700

    commiting initial intelliJ

---
+++ src/main/java/com/revature/ghiblihub/GhibliHubApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ghiblihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhibliHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhibliHubApplication.class, args);
+	}
+
+}
