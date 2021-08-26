b52eaad3a0b4f0ad968102c249991b730f46d8ae
DouglasRamirez1 <douglasram4018@gmail.com>
2021-08-26 11:38:31 -0500

    Review Model created

--- src/main/java/com/revature/ghiblihub/models/Review.java
+++ src/main/java/com/revature/ghiblihub/models/Review.java
@@ -1,4 +1,35 @@
 package com.revature.ghiblihub.models;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reviews")
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Review {
+
+    @Id
+    @Column(name = "review_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reviewId;
+
+    @Column(name = "rating")
+    private float rating;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(name = "film_id", nullable = false)
+    private int filmId;
+
 }
