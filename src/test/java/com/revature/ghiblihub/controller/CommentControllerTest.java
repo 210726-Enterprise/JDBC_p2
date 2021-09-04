c754449258f2f3b7257051c2be27a87daae30eb0
Chris Zhang <christopher.zhang@revature.net>
2021-09-04 14:41:21 -0700

    added images for filmDetails and testing skeleton

---
+++ src/test/java/com/revature/ghiblihub/controller/CommentControllerTest.java
@@ -0,0 +1,12 @@
+package com.revature.ghiblihub.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
