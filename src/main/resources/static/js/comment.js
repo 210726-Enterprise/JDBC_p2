184c78ed506351c8464fac34ac3535aafea568f5
Jintao Lin <jtlin226@gmail.com>
2021-09-09 22:43:55 -0500

    Comment page layout, documentation on javascript files, removed profile tab from navigation bar

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -1,25 +1,45 @@
+
+// the action url for commentForm to execute when submitting the form
 const actionURL = `${window.location.href.substr(0, window.location.href.indexOf('?'))}`;
+
+// the api url that returns single review with current reviewId in the endpoint
 const reviewURL = "http://" + window.location.hostname + ":" + window.location.port + window.location.pathname + "/review";
+
+// the api url that returns all associated comments of the current review
 const commentURL = "http://" + window.location.hostname + ":" + window.location.port + window.location.pathname + "/comments";
 
-
+// the function that populates the HTML page with the review object fetched from reviewURL
 function populateReview(review){
     document.getElementById("currentReview").innerHTML =
-    `<h4>by: ${review.user.username} &emsp; rating: ${review.rating}</h4>
-        <hr>
-    <p>${review.content}</p>`;
+    `<div class="reviewContent">
+        <div class="innerReviewContent">
+            <h4>by: ${review.user.username} &emsp; rating: ${review.rating}</h4>
+            <hr>
+            <p>${review.content}</p>
+        </div>
+    </div>`;
 }
+
+// the function that populates the HTML with comments of the current review fetched from commentURL
 function populateComments(comments) {
+    // loop over the data to append every comment to the page
     for(comment of comments) {
+        // create element to be append to the page
         let post = document.createElement('div');
         post.className = "commentContent";
-        post.innerHTML = `<h4>${comment.user.username}</h4>
-                            <p>${comment.content}</p>`
-
+        post.innerHTML = `<div class="innerCommentContent">
+                                <h4>${comment.user.username}</h4>
+                                <hr>
+                                <p>${comment.content}</p>
+                            </div>`
+        // append to the static div predefined in the HTML page
         document.getElementById("allComments").append(post);
     }
 }
 
+// performs nested fetch that populates the HTML page
+// with one review and all comments of that review
+// when the web page loads
 window.onload = () => {
     console.log(window.location.href + "/review");
     console.log(window.location.href + "/comments");
